888e937850a7fb62e095825b881c80804421918c
Vivienne Lovelock <viv.lovelock@gmail.com>
2015-09-22 14:53:46 +1000

    Create user sign up
    Created most of user sign up and password, will add admin powers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@
 #
 
 class User < ActiveRecord::Base
+	has_secure_password
 	has_many :reservations
 	has_many :flights, :through => :reservations
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+	root :to => 'static_pages#home'
+	
+  get 'static_pages/home'
+
+
   resources :reservations
   resources :users
   resources :flights
