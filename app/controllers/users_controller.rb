db18dfc784d04aee0415431bb094f0b4b670d3a1
veebuv <veebuv@gmail.com>
2015-09-22 16:18:32 +1000

    got sessions and login on signup running

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,12 +28,15 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
 	root :to => 'static_pages#home'
 
   resources :reservations
   resources :users
   resources :flights
   resources :planes
+
+ get '/login' => 'session#new'
+ post '/login' => 'session#create'
+ delete '/login' => 'session#destroy'
 
   get '/search' => 'pages#search'
 end
